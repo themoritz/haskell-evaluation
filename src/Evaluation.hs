23c303525fdd7860c0f6c726467b4878fdcf1265
Moritz Drexl <mdrexl@fastmail.fm>
2019-11-19 22:46:48 -0800

    more stuff

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -9,6 +9,8 @@ import Debug.Trace (trace)
 
 data Foo a = Foo a
 
+-- >>> :set -XBangPatterns
+
 caf :: Foo (Foo Int)
 caf = Foo (Foo 1)
 
@@ -22,6 +24,13 @@ f !i =
 g :: Int -> Int
 g i =
   trace "g" (i + 1)
+
+-- >>> let x = True
+-- >>> :sprint x
+-- x = True
+-- >>> let !y = 1 + 1 :: Int
+-- >>> :sprint y
+-- y = 2
 
 -- >>> _ <- evaluate caf
 -- >>> :sprint caf
@@ -50,3 +59,13 @@ foldl f z (x:xs) =
     z' = z `f` x
   in
     foldl f z' xs
+
+
+mean :: [Double] -> Double
+mean xs = sum / count
+  where
+    (sum, count) = foldl step (0, 0) xs
+    step (sumAcc, countAcc) x = (sumAcc + x, countAcc + 1)
+
+-- >>> mean [2,3]
+-- 2.5
